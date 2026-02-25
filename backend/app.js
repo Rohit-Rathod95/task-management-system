3867365164cf9a9a09f84a573f5e74c9013fec71
Rohit Rathod <rohit1212rathod@gmail.com>
2026-02-25 19:13:28 +0530

    Client URL

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-    origin: process.env.CLIENT_URL || "http://localhost:3000",
+    origin: process.env.CLIENT_URL,
     credentials: true,
 }));
 
